31be7d20accf8c3dfbc437ae3e52c5d3fdac2c46
Musah Abdul-Malik <musah.abdulmalikgh@gmail.com>
2021-04-10 22:01:32 +0000

    Handle 404 routes

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express')
-
+const morgan = require('morgan')
 const app = express()
 
 const productsRoute = require('./api/routes/products')
 
 const ordersRoute = require('./api/routes/orders')
 
+// middlewares 
+app.use(morgan('dev'))
 app.use('/products', productsRoute)
 app.use('/orders', ordersRoute)
 
+// not found routes 
+app.use((req, res, next) => {
+    const error = new Error("Not Found")
+    error.status = 404
+    next(error)
+})
+
+// database errors
+app.use((error,req, res, next)=> {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+} )
 module.exports = app
